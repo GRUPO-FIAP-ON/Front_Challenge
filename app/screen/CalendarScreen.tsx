9869de2eff0cd7cf773b2df3623f3151ef40a6a1
ricardo-temple <ricardovtemple@gmail.com>
2024-09-02 23:05:51 -0300

    add calendar to CalendarScreen.tsx and React-Calendar dependencies

--- app/screen/CalendarScreen.tsx
+++ app/screen/CalendarScreen.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import { View, Text, StyleSheet } from 'react-native';
 
 const CalendarScreen: React.FC = () => {
+  const [selected, setSelected] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Tela do Calendário</Text>
+    <View>
+      <Text
+      style={styles.header}>Calendário</Text>
+      <Calendar
+    style={styles.calendar}
+      theme={{
+      backgroundColor: '#ffffff',
+      calendarBackground: '#ffffff',
+      textSectionTitleColor: '#b6c1cd',
+      selectedDayBackgroundColor: '#5218fa',
+      selectedDayTextColor: '#ffffff',
+      todayTextColor: '#5218fa',
+      dayTextColor: '#2d4150', 
+      arrowColor: 'purple'
+    }}
+      onDayPress={(day: { dateString: React.SetStateAction<string>; }) => {
+        setSelected(day.dateString);
+      }}
+      markedDates={{
+        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'purple'}
+      }}
+    />
     </View>
   );
 };
@@ -19,6 +42,23 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontFamily: 'Ubuntu_400Regular',
   },
+  calendar:{
+    borderWidth:  1,
+    borderColor: 'purple',
+    marginHorizontal: 6,
+    borderRadius: 10,
+  },
+  header:{
+    fontSize: 18,
+    fontFamily: 'Ubuntu',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingTop: 60,
+    marginBottom: 12,
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 30,
+  }
 });
 
 export default CalendarScreen;
