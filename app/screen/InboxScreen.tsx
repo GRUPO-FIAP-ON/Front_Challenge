433cca5dafc7baddd65baf5291a319b66a2bc45c
Guilherme Avelino <guilhermeavelino1@outlook.com>
2024-09-16 14:03:38 -0300

    feat: add emails integration

--- app/screen/InboxScreen.tsx
+++ app/screen/InboxScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { FlatList, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, View, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
 import EmailItem from '../components/EmailItem';
 import Footer from '../components/Footer';
 import { Ionicons } from '@expo/vector-icons';
@@ -15,23 +15,33 @@ type RootStackParamList = {
 
 type InboxScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Inbox'>;
 
-const emailsEstaSemana = [
-  { id: '1', sender: 'Thábata Orbeteli', subject: 'Assunto', preview: 'Lorem ipsum dolor...', date: '08/07/2024', flagged: false },
-  { id: '2', sender: 'Yago Taguchi', subject: 'Assunto', preview: 'Lorem ipsum dolor...', date: '09/07/2024', flagged: true },
-];
-
-const emailsSemanaPassada = [
-  { id: '3', sender: 'Eduardo Shoiti', subject: 'Assunto', preview: 'Lorem ipsum dolor...', date: '01/07/2024', flagged: false },
-];
-
-const emailsEsteMes = [
-  { id: '3', sender: ' Guilherme Avelino', subject: 'Assunto', preview: 'Lorem ipsum dolor...', date: '01/07/2024', flagged: false },
-];
-
 const InboxScreen: React.FC = () => {
   const navigation = useNavigation<InboxScreenNavigationProp>();
+  const [emails, setEmails] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
   const [newEmailVisible, setNewEmailVisible] = useState(false);
   const { isDarkTheme } = useTheme(); 
+
+  useEffect(() => {
+    const fetchEmails = async () => {
+      try {
+        const userId = 1;
+        const response = await fetch(`http://localhost:3000/users/${userId}/emails`);
+        
+        if (!response.ok) {
+          throw new Error('Erro ao buscar emails');
+        }
+        const data = await response.json();
+        setEmails(data);
+      } catch (error) {
+        console.error("Erro ao buscar e-mails: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEmails();
+  }, []);
 
   const renderEmailItem = ({ item }: any) => (
     <EmailItem
@@ -43,44 +53,28 @@ const InboxScreen: React.FC = () => {
     />
   );
 
+  if (loading) {
+    return <ActivityIndicator size="large" color="#5138EE" />;
+  }
+
   return (
     <View style={[styles.container, { backgroundColor: isDarkTheme ? '#333' : '#f8f8f8' }]}>
-    <FlatList
-      data={emailsEstaSemana}
-      renderItem={renderEmailItem}
-      keyExtractor={(item) => item.id}
-      ListHeaderComponent={
-        <Text style={[styles.sectionHeader, { color: isDarkTheme ? '#FFFFFF' : '#000', backgroundColor: isDarkTheme ? '#444' : '#f0f0f0' }]}>
-          Esta Semana
-        </Text>
-      }
-    />
-    <FlatList
-      data={emailsSemanaPassada}
-      renderItem={renderEmailItem}
-      keyExtractor={(item) => item.id}
-      ListHeaderComponent={
-        <Text style={[styles.sectionHeader, { color: isDarkTheme ? '#FFFFFF' : '#000', backgroundColor: isDarkTheme ? '#444' : '#f0f0f0' }]}>
-          Semana Passada
-        </Text>
-      }
-    />
-    <FlatList
-      data={emailsEsteMes}
-      renderItem={renderEmailItem}
-      keyExtractor={(item) => item.id}
-      ListHeaderComponent={
-        <Text style={[styles.sectionHeader, { color: isDarkTheme ? '#FFFFFF' : '#000', backgroundColor: isDarkTheme ? '#444' : '#f0f0f0' }]}>
-          Este Mês
-        </Text>
-      }
-    />
-    <TouchableOpacity 
-      style={[styles.floatingButton, { backgroundColor: isDarkTheme ? '#5138EE' : '#5138EE' }]}
-      onPress={() => setNewEmailVisible(true)}
-    >
-      <Ionicons name="add" size={30} color="#FFFFFF" />
-    </TouchableOpacity>
+      <FlatList
+        data={emails}
+        renderItem={renderEmailItem}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={
+          <Text style={[styles.sectionHeader, { color: isDarkTheme ? '#FFFFFF' : '#000', backgroundColor: isDarkTheme ? '#444' : '#f0f0f0' }]}>
+            E-mails
+          </Text>
+        }
+      />
+      <TouchableOpacity 
+        style={[styles.floatingButton, { backgroundColor: isDarkTheme ? '#5138EE' : '#5138EE' }]}
+        onPress={() => setNewEmailVisible(true)}
+      >
+        <Ionicons name="add" size={30} color="#FFFFFF" />
+      </TouchableOpacity>
 
       {newEmailVisible && (
         <NewEmail onClose={() => setNewEmailVisible(false)} />
