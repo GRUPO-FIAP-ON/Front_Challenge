52bfe7b7f750c70de7825c13c9a0d7ed01e4eb81
Guilherme Avelino <guilhermeavelino1@outlook.com>
2024-09-18 16:58:33 -0300

    feat: add sent emails

--- app/screen/EmailSentScreen.tsx
+++ app/screen/EmailSentScreen.tsx
@@ -1,10 +1,101 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useSession } from '../context/SessionContext';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import NewEmail from './NewEmail';
+import Footer from '../components/Footer';
+import EmailItem from '../components/EmailItem';
+import { useTheme } from '../context/ThemeContext'; 
+
+type RootStackParamList = {
+  Inbox: undefined;
+  NovoEmail: undefined;
+};
+
+type InboxScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Inbox'>;
 
 const EmailSentScreen: React.FC = () => {
+  const navigation = useNavigation<InboxScreenNavigationProp>();
+  const [emails, setEmails] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [newEmailVisible, setNewEmailVisible] = useState(false);
+  const { isDarkTheme } = useTheme(); 
+  const { user, loading: userLoading } = useSession();
+
+  useEffect(() => {
+    const fetchEmails = async () => {
+      if (!user || userLoading) return;
+
+      try {
+        const response = await fetch(`http://localhost:3000/users/${user.id}/emails`);
+        
+        if (!response.ok) {
+          throw new Error('Erro ao buscar e-mails');
+        }
+        const data = await response.json();
+        setEmails(data.sentEmails);
+      } catch (error) {
+        console.error("Erro ao buscar e-mails: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEmails();
+  }, [user, userLoading]);
+
+  const renderEmailItem = ({ item }: any) => (
+    <EmailItem
+      sender={item.sender.fullName}
+      subject={item.subject}
+      preview={item.body}
+      date={item.date}
+      flagged={item.flagged} 
+    />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Tela de Emails Enviados</Text>
+    <View style={[styles.container, { backgroundColor: isDarkTheme ? '#333' : '#f8f8f8' }]}>
+      {
+        loading || userLoading ? (
+          <View style={[{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%'
+          }]}>
+            <ActivityIndicator size="large" color="#5138EE" />
+          </View>
+        ) : (
+          <>
+            <FlatList
+              data={emails}
+              renderItem={renderEmailItem}
+              keyExtractor={(item) => item.id}
+              ListHeaderComponent={
+                <Text style={[styles.sectionHeader, { color: isDarkTheme ? '#FFFFFF' : '#000', backgroundColor: isDarkTheme ? '#444' : '#f0f0f0' }]}>
+                  E-mails enviados
+                </Text>
+              }
+            />
+            <TouchableOpacity 
+              style={[styles.floatingButton, { backgroundColor: isDarkTheme ? '#5138EE' : '#5138EE' }]}
+              onPress={() => setNewEmailVisible(true)}
+            >
+              <Ionicons name="add" size={30} color="#FFFFFF" />
+            </TouchableOpacity>
+
+            {newEmailVisible && (
+              <NewEmail onClose={() => setNewEmailVisible(false)} />
+            )}
+
+            <Footer />
+          </>
+        )
+      }
     </View>
   );
 };
@@ -12,12 +103,20 @@ const EmailSentScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  text: {
-    fontSize: 18,
-    fontFamily: 'Ubuntu_400Regular',
+  sectionHeader: {
+    padding: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+    fontFamily: 'Ubuntu_700Bold',
+  },
+  floatingButton: {
+    position: 'absolute',
+    right: 20,
+    bottom: 80, 
+    borderRadius: 50,
+    padding: 15,
+    elevation: 5,
   },
 });
 
